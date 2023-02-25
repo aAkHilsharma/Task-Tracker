d705bc610e3e7c9a5a3143897b294aae98fa4b90
akhil <akhilsharma6721@gmail.com>
2023-02-25 21:14:15 +0530

    added api request to fetch tasks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 function App() {
   const [showAdd, setShowAdd] = useState(false);
   const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    const getTasks = async ()=>{
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    }
 
+    getTasks()
+  }, [])
+  const fetchTasks = async ()=>{
+    const res =  await fetch("http://localhost:5000/tasks");
+    const data = await res.json();
+    return data;
+  }
   const addTask = (task) => {
     const id = Math.floor(Math.random()*10000+1);
     const newTask = {id , ...task};
