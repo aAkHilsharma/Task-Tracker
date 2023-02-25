b7cf71595fbec2c0d13f4c4d19617a73705a9af7
akhil <akhilsharma6721@gmail.com>
2023-02-25 15:52:30 +0530

    added component/Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Initial Commit</h1>
+      <Header />
     </div>
   );
 }
