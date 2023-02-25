2032d2041d3be9a5f326de2dea0550c7a0bce377
akhil <akhilsharma6721@gmail.com>
2023-02-25 18:30:06 +0530

    created delete feature

--- src/App.js
+++ src/App.js
@@ -23,10 +23,16 @@ function App() {
         reminder: "false"
     },
   ])
+
+  const deleteTask = (id)=>{
+    setTasks(tasks.filter((task)=>{
+      return task.id !== id;
+    }))  
+  }
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   );
 }
