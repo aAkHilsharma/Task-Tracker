1acc137e5a49e068b6cefaf8c48a7c83d43bf156
akhil <akhilsharma6721@gmail.com>
2023-02-25 17:49:37 +0530

    added task component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id : "1",
+        text: "Doctr's Appointment",
+        reminder: "true"
+    },
+    {
+        id : "2",
+        text: "Study Java",
+        reminder: "true"
+    },
+    {
+        id : "3",
+        text: "Eat healthy Food",
+        reminder: "false"
+    },
+  ])
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 
-const Button = ({color, text}) => {
-  return <button style={{backgroundColor: color}} className='btn'>{text}</button>
+const Button = ({color, text, onClick}) => {
+  return <button onClick={onClick} style={{backgroundColor: color}} className='btn'>{text}</button>
 }
 
 Button.defaultProps= {
@@ -10,6 +10,7 @@ Button.defaultProps= {
 }
 Button.propTypes = {
     color : PropTypes.string,
-    text : PropTypes.string
+    text : PropTypes.string,
+    onClick: PropTypes.func,
 }
 export default Button
